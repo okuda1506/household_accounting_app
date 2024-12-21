66d5295dcc83737936069bcf354f49579f9db0d5
okuda1506 <okuda1506@gmail.com>
2024-12-21 11:52:07 +0900

    PaymentMethodモデルの作成

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -9,4 +9,30 @@ class PaymentMethod extends Model
 {
     /** @use HasFactory<\Database\Factories\PaymentMethodFactory> */
     use HasFactory;
+
+    /**
+     * 一括代入を許可する属性を指定する
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'deleted',
+    ];
+
+    /**
+     * DBから取得した値やモデルで使用する値を指定した型に自動変換する
+     *
+     * @return array<string, string>
+     */
+    protected $casts = [
+        'deleted' => 'boolean',
+    ];
+
+    // 支払方法削除
+    public function delete(): void
+    {
+        $this->deleted = true;
+        $this->save();
+    }
 }
