032e0f8d87fb59f51ad48c4d26125dd8a77defe2
okuda1506 <okuda1506@gmail.com>
2025-01-01 17:12:37 +0900

    カテゴリと取引タイプのシーディング作成

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'user_id' => 1,
+            'name' => '',
+            'transaction_types_id' => 1,
+            'sort_no' => 1,
+            'deleted' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
--- database/seeders/TransactionTypeSeeder.php
+++ database/seeders/TransactionTypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TransactionType;
 
 class TransactionTypeSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class TransactionTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        TransactionType::create([
+            'name' => '収入',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        TransactionType::create([
+            'name' => '支出',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
