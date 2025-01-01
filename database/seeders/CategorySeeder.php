4bbd733ab932bebb0cf8f1c634444474309669e2
okuda1506 <okuda1506@gmail.com>
2025-01-01 18:36:11 +0900

    CategorySeedの作成

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,9 +15,45 @@ class CategorySeeder extends Seeder
     {
         Category::create([
             'user_id' => 1,
-            'name' => '',
-            'transaction_types_id' => 1,
+            'name' => '給与',
+            'transaction_type_id' => 1,
             'sort_no' => 1,
+            'deleted' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            'user_id' => 1,
+            'name' => '副業収入',
+            'transaction_type_id' => 1,
+            'sort_no' => 2,
+            'deleted' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            'user_id' => 1,
+            'name' => '食費',
+            'transaction_type_id' => 2,
+            'sort_no' => 1,
+            'deleted' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            'user_id' => 1,
+            'name' => '交通費',
+            'transaction_type_id' => 2,
+            'sort_no' => 2,
+            'deleted' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Category::create([
+            'user_id' => 1,
+            'name' => '家賃',
+            'transaction_type_id' => 2,
+            'sort_no' => 3,
             'deleted' => false,
             'created_at' => now(),
             'updated_at' => now(),
