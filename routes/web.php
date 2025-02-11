5b937a5da4160a123921c14737939e1bdae28c4b
okuda1506 <okuda1506@gmail.com>
2025-02-11 15:39:37 +0900

    ログイン周りのリダイレクト修正

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware(['auth', 'verified'])->name('index');
 
+// ログイン・ログアウト用に一旦残しておく
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
