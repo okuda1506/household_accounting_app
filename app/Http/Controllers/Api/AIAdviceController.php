dbd08bf548b9ea78393beaaf2d73ded1729f21d6
okuda1506 <okuda1506@gmail.com>
2026-03-07 11:45:39 +0900

    Controllerの実装

--- app/Http/Controllers/Api/AIAdviceController.php
+++ app/Http/Controllers/Api/AIAdviceController.php
@@ -2,26 +2,45 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
-use App\Services\DashboardService;
+use App\Services\AiAdviceService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use App\Ai\Agents\SalesCoach;
+use Illuminate\Http\Response;
+use App\Http\Resources\AiAdviceResource;
+use Illuminate\Support\Facades\Log;
 
 class AIAdviceController extends Controller
 {
-    public function aiAdvice(Request $request): JsonResponse
+    private AiAdviceService $aiAdviceService;
+
+    public function __construct(AiAdviceService $aiAdviceService)
     {
-        $user = $request->user();
+        $this->aiAdviceService = $aiAdviceService;
+    }
+    /**
+     * AIアドバイスを取得する
+     *
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            $dto = $this->aiAdviceService->getAdvice(auth()->id());
+        } catch (\Throwable $e) {
+            Log::error($e);
 
-        // todo: 分析用データ集計
-        // todo: AI呼び出し
-        $salesCoach = new SalesCoach();
-        $response = $salesCoach->prompt('newjeansでおすすめの曲は？');
-        dump($response);exit;
+            return ApiResponse::error(
+                null,
+                [__('messages.ai_advice_fetch_failed') . ': ' . $e->getMessage()],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
 
-        return response()->json([
-            'analysis' => $analysisText
-        ]);
+        return ApiResponse::success(
+            new AiAdviceResource($dto),
+            __('messages.ai_advice_fetched')
+        );
     }
 }
