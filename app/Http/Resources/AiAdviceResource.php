cf807354c519e15964bdc86dfd598ca3fdda3d29
okuda1506 <okuda1506@gmail.com>
2026-03-08 18:22:23 +0900

    レビュー修正

--- app/Http/Resources/AiAdviceResource.php
+++ app/Http/Resources/AiAdviceResource.php
@@ -13,22 +13,7 @@ class AiAdviceResource extends JsonResource
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
-    {
-        return [
-            'risk_level' => $this->riskLevel->value(),
-            'analysis' => [
-                'budget_gap' => $this->analysis->budgetGap,
-                'daily_safe_limit' => $this->analysis->dailySafeLimit,
-                'main_issue_category' => $this->analysis->mainIssueCategory,
-                'analysis_reason' => $this->analysis->analysisReason,
-            ],
-            'pattern' => $this->pattern,
-            'advice' => [
-                'micro_action' => $this->advice->microAction,
-                'daily_budget_target' => $this->advice->dailyBudgetTarget,
-                'focus_category' => $this->advice->focusCategory,
-            ],
-            'motivation' => $this->motivation,
-        ];
-    }
+{
+    return $this->resource->toArray();
+}
 }
